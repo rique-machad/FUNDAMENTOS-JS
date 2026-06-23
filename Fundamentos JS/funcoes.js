79974f81f3b1a80d3badd805ed9708908f3955ea
rique-machad <henrique.mendonca.machado@escola.pr.gov.br>
2026-06-23 10:30:41 -0300

    inicio

---
+++ Fundamentos JS/funcoes.js
@@ -0,0 +1,15 @@
+//let x = "";
+//x = "oi";
+//console.log(x);
+
+
+function imprimeTexto(texto){
+    console.log(texto);
+}
+
+imprimeTexto("oi mundo!");
+imprimeTexto(soma());
+
+function soma(){
+    return 2 + 2;
+}
